b8dc4337005afb1fe822ca1af10f367350f0c610
Zach <shaw.zacht@gmail.com>
2022-03-19 10:06:32 -0700

    commit tree 189924d12700cee4712b41abb102da875fcc139a

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Write your code here
+class Breakfast {
+    constructor (food, drink){
+        this.food = food;
+        this.drink = drink;
+    }
+}
+
+class Lunch {
+    constructor (salad, soup, drink) {
+        this.salad = salad;
+        this.soup = soup
+        this.drink = drink;
+    }
+}
+
+class Dinner {
+    #dessert = 'creme brule';
+    constructor (salad, soup, entree){
+        this.salad = salad;
+        this.soup = soup;
+        this.entree = entree
+    }
+}
+
+const newDinner = new dinner ('steak', 'wine', 'creme brulee');
+console.log(newDinner)
